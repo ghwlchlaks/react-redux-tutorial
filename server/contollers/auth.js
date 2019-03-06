07730f0c1b1f69a4083c038bdd0fdc9688953236
ghwlchlaks <ghwlchlaks@naver.com>
2019-03-06 20:15:04 +0900

    로그인 기능 구현

--- server/contollers/auth.js
+++ server/contollers/auth.js
@@ -55,8 +55,43 @@ router.post('/signup', (req, res) => {
   });
 });
 
+/**
+ * error code
+ * 1: login failed
+ */
 router.post('/signin', (req, res) => {
-  res.send({ info: null });
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (typeof password !== 'string') {
+    return res.status(401).send({
+      error: 'Login failed',
+      code: 1
+    });
+  }
+
+  User.findOne({ username: username }, (err, findUser) => {
+    if (err) throw err;
+    // 존재하는 아이디인지 검사
+    if (!findUser) {
+      return res.status(401).send({
+        error: 'login failed',
+        code: 1
+      });
+    }
+
+    // session 생성
+    const session = req.session;
+    session.loginInfo = {
+      _id: findUser._id,
+      username: findUser.username
+    };
+
+    // return success
+    return res.send({
+      success: true
+    });
+  });
 });
 
 router.get('/getInfo', (req, res) => {
