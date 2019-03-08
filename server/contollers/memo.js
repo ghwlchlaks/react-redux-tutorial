724c4ddf8f139b25f1c29b1ef96d64caae569c7d
ghwlchlaks <ghwlchlaks@naver.com>
2019-03-08 19:31:39 +0900

    글 삭제 기능 추가 route=/api/memo/:id DELETE

--- server/contollers/memo.js
+++ server/contollers/memo.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Memo = require('../models/memo').Memo;
+const mongoose = require('mongoose');
 
 // write memo
 router.post('/', (req, res) => {
@@ -41,7 +42,47 @@ router.put('/:id', (req, res) => {});
 
 //delete memo
 router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  const loginInfo = req.session.loginInfo;
+
   // check memo id valid
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({
+      error: 'invalid id',
+      code: 1
+    });
+  }
+
+  // check login
+  if (typeof loginInfo === 'undefined') {
+    return res.status(403).send({
+      error: 'not logged in',
+      code: 2
+    });
+  }
+
+  // find memo and check for writer
+  Memo.findById(id, (err, memo) => {
+    if (err) throw err;
+    if (!memo) {
+      return res.status(404).send({
+        error: 'no resource',
+        code: 3
+      });
+    }
+    if (memo.writer != loginInfo.username) {
+      return res.status(403).send({
+        error: 'permission failure',
+        code: 4
+      });
+    }
+
+    // remove memo
+    Memo.remove({ _id: id }, err => {
+      if (err) throw err;
+      res.send({ success: true });
+    });
+  });
 });
 
 //get memo list
