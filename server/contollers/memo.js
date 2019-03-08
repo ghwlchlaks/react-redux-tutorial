34bcb477b63984ca7fcdc94a042f24531c343934
ghwlchlaks <ghwlchlaks@naver.com>
2019-03-08 19:24:24 +0900

    글 작성 기능 추가 route = /api/memo POST

--- server/contollers/memo.js
+++ server/contollers/memo.js
@@ -3,7 +3,38 @@ const router = express.Router();
 const Memo = require('../models/memo').Memo;
 
 // write memo
-router.post('/', (req, res) => {});
+router.post('/', (req, res) => {
+  const contents = req.body.contents;
+  const loginInfo = req.session.loginInfo;
+
+  // login check
+  if (typeof loginInfo === 'undefined') {
+    return res.status(403).send({
+      error: 'not logged in',
+      code: 1
+    });
+  }
+
+  // check contents valid
+  if (typeof contents !== 'string' || contents === '') {
+    return res.status(400).send({
+      error: 'empty contents',
+      code: 2
+    });
+  }
+
+  // create new memo
+  const memo = new Memo({
+    writer: loginInfo.username,
+    contents: contents
+  });
+
+  // save memo
+  memo.save(err => {
+    if (err) throw err;
+    return res.send({ success: true });
+  });
+});
 
 // modify memo
 router.put('/:id', (req, res) => {});
